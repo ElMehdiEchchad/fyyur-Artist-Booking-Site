786f0cacecd0121af665b9706a1f189b6e03b7ba
ElMehdiEchchad <58845064+ElMehdiEchchad@users.noreply.github.com>
2022-08-21 16:27:55 +0100

    added configurations for workspace project

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@localhost:5432/dbFyuur' #using docker postgreSQL container 
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@127.0.0.0:5432/dbfyuur'
